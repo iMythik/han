0c136259d6ec44a81a47711d4e113ba02149fdaa
iMythik <iMythik@gmail.com>
2018-03-28 22:24:26 -0400

    fixed drawing bug

--- simple_vi/main.lua
+++ simple_vi/main.lua
@@ -193,7 +193,7 @@ end
 -- Draw hook
 
 local function ondraw()
-	local pos = graphics.world_to_screen(player.pos)
+	if not player.isVisible then return end
 	graphics.draw_circle(player.pos, q_range(), 2, graphics.argb(255, 192, 57, 43), 70)
 end
 
